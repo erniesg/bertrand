b5ec16aff3fd9abd6af4c09c3938c3b542aed233
erniesg <erniesg@users.noreply.github.com>
2023-07-28 22:05:17 +0800

    updating memory branch

--- server/main.py
+++ server/main.py
@@ -1,6 +1,7 @@
 import os
 from typing import Optional
 import uvicorn
+from starlette.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, File, Form, HTTPException, Depends, Body, UploadFile
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from fastapi.staticfiles import StaticFiles
@@ -20,7 +21,34 @@ from services.file import get_document_from_file
 from models.models import DocumentMetadata, Source
 
 bearer_scheme = HTTPBearer()
-BEARER_TOKEN = os.environ.get("BEARER_TOKEN")
+
+from dotenv import load_dotenv
+import os
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+
+# Load the .env file
+load_dotenv()
+
+# Get the Key Vault URL from an environment variable
+KEY_VAULT_URL = os.getenv('KEY_VAULT_URL')
+logger.info(f"Loaded KEY_VAULT_URL: {KEY_VAULT_URL}")
+
+# Create a credential object using the DefaultAzureCredential class
+credential = DefaultAzureCredential()
+
+# Create a SecretClient object
+secret_client = SecretClient(vault_url=KEY_VAULT_URL, credential=credential)
+
+# Retrieve the secrets
+BEARER_TOKEN = secret_client.get_secret("bearer-token").value
+logger.info(f"Loaded BEARER_TOKEN: {BEARER_TOKEN[:5]}...")  # Log the first 5 characters of the BEARER_TOKEN for privacy reasons
+
+OPENAI_API_KEY = secret_client.get_secret("openai-api").value
+logger.info(f"Loaded OPENAI_API_KEY: {OPENAI_API_KEY[:5]}...")  # Log the first 5 characters of the OPENAI_API_KEY for privacy reasons
+
+DATASTORE = os.getenv('DATASTORE')
+logger.info(f"Loaded DATASTORE: {DATASTORE}")
 assert BEARER_TOKEN is not None
 
 
@@ -30,7 +58,14 @@ def validate_token(credentials: HTTPAuthorizationCredentials = Depends(bearer_sc
     return credentials
 
 
-app = FastAPI(dependencies=[Depends(validate_token)])
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://chat.openai.com"],  # Allow specific origin (in this case, OpenAI)
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
 app.mount("/.well-known", StaticFiles(directory=".well-known"), name="static")
 
 # Create a sub-application, in order to access just the query endpoint in an OpenAPI schema, found at http://0.0.0.0:8000/sub/openapi.json when the app is running locally
@@ -38,7 +73,7 @@ sub_app = FastAPI(
     title="Retrieval Plugin API",
     description="A retrieval API for querying and filtering documents based on natural language queries and metadata",
     version="1.0.0",
-    servers=[{"url": "https://your-app-url.com"}],
+    servers=[{"url": "http://0.0.0.0:8000"}],
     dependencies=[Depends(validate_token)],
 )
 app.mount("/sub", sub_app)
@@ -75,8 +110,27 @@ async def upsert_file(
     "/upsert",
     response_model=UpsertResponse,
 )
+async def upsert_main(
+    request: UpsertRequest = Body(...),
+    token: HTTPAuthorizationCredentials = Depends(validate_token),
+):
+    try:
+        ids = await datastore.upsert(request.documents)
+        return UpsertResponse(ids=ids)
+    except Exception as e:
+        logger.error(e)
+        raise HTTPException(status_code=500, detail="Internal Service Error")
+
+
+@sub_app.post(
+    "/upsert",
+    response_model=UpsertResponse,
+    # NOTE: We are describing the shape of the API endpoint input due to a current limitation in parsing arrays of objects from OpenAPI schemas. This will not be necessary in the future.
+    description="Save chat information. Accepts an array of documents with text (potential questions + conversation text), metadata (source 'chat' and timestamp, no ID as this will be generated). Confirm with the user before saving, ask for more details/context.",
+)
 async def upsert(
     request: UpsertRequest = Body(...),
+    token: HTTPAuthorizationCredentials = Depends(validate_token),
 ):
     try:
         ids = await datastore.upsert(request.documents)
@@ -92,6 +146,7 @@ async def upsert(
 )
 async def query_main(
     request: QueryRequest = Body(...),
+    token: HTTPAuthorizationCredentials = Depends(validate_token),
 ):
     try:
         results = await datastore.query(
@@ -111,6 +166,7 @@ async def query_main(
 )
 async def query(
     request: QueryRequest = Body(...),
+    token: HTTPAuthorizationCredentials = Depends(validate_token),
 ):
     try:
         results = await datastore.query(
@@ -128,6 +184,7 @@ async def query(
 )
 async def delete(
     request: DeleteRequest = Body(...),
+    token: HTTPAuthorizationCredentials = Depends(validate_token),
 ):
     if not (request.ids or request.filter or request.delete_all):
         raise HTTPException(
